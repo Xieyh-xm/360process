0f754266950269a88bdd70e81c07bd737c986938
Yuhong Xie <xieyh0823@163.com>
2022-10-06 19:09:24 +0800

    update files

--- m4s_to_trace.py
+++ m4s_to_trace.py
@@ -8,18 +8,21 @@ from _ctypes import PyObj_FromPtr
 '''
 用于读取m4s的目录生成video_meta_data
 '''
-
-TILE_NUM = 8
+VIDEO_NAME = "1-5-TahitiSurf"
+TILE_NUM = 64
 SEGMENT_DURATION_MS = 1000
-BITRATE_KBPS = [600, 1500, 3000]
-BITRATE_LEVEL = len(BITRATE_KBPS)
+BITRATE_KBPS = [50, 150, 250, 400, 550, 750]
+BITRATE_KBPS_LOW = [50, 150, 250]  # kbps
+BITRATE_KBPS_HIGH = [400, 550, 750]  # kbps
+
+BITRATE_LEVEL = len(BITRATE_KBPS_LOW)
 
 
 def get_tile_size(inputPath: str):
     '''
     读取m4s文件的大小，获得各个tile的码率
     :param path: dash m4s文件的存储路径
-    :return: 保存每个tile大小的列表
+    :return: 保存每个tile大小的数组
     '''
     sizeList = []  # [tile][bitrate][segment]
 
@@ -29,11 +32,21 @@ def get_tile_size(inputPath: str):
     for filename in fileList:
         if "chunk-stream" in filename:  # 只取类chunk-stream0-00003.m4s的文件
             tile_stream_list.append(filename)
+
     # print(tile_stream_list)
 
     # 对文件进行重新排序
-    tile_stream_list.sort(key=lambda x: int(x[12:13] if x[13] == '-' else x[12:14]))
-    # print(tile_stream_list)
+    def take_num(elem):
+        if elem[13] == '-':
+            return int(elem[12:13])
+        elif elem[14] == '-':
+            return int(elem[12:14])
+        elif elem[15] == '-':
+            return int(elem[12:15])
+
+    # tile_stream_list.sort(key=lambda x: int(x[12:13] if x[13] == '-' else x[12:14]))
+    tile_stream_list.sort(key=take_num)
+    print(tile_stream_list)
 
     segment_num = len(tile_stream_list) / (TILE_NUM * BITRATE_LEVEL)
     # print("segment_num = " + str(segment_num))
@@ -41,7 +54,7 @@ def get_tile_size(inputPath: str):
     segment_per_bitrate = []
     bitrate_per_tile = []
     for idx, tilename in enumerate(tile_stream_list):
-        fileSize = os.path.getsize(os.path.join(inputPath, tilename))
+        fileSize = os.path.getsize(os.path.join(inputPath, tilename)) * 8  # bps
         segment_per_bitrate.append(fileSize)
         if len(segment_per_bitrate) == segment_num:
             bitrate_per_tile.append(segment_per_bitrate[:])
@@ -52,7 +65,7 @@ def get_tile_size(inputPath: str):
     sizeList = np.array(sizeList)
     sizeList = sizeList.transpose(2, 0, 1)  # [segment][tile][bitrate]
     # print(sizeList)
-    return sizeList.tolist()
+    return sizeList
 
 
 def generate_video_trace_json(size, outputPath):
@@ -71,35 +84,51 @@ def generate_video_trace_json(size, outputPath):
 
     movie360_json_data = json.dumps(video_trace_dict, indent=4, separators=(',', ':'))
     # movie360_json_data = json.dumps(video_trace_dict)
-    with open(outputPath + "movie360.json", 'w') as json_file:
+    with open(outputPath + VIDEO_NAME + ".json", 'w') as json_file:
         json_file.write(movie360_json_data)
     return
 
 
 def generate_video_trace_csv(size_of_tile, outputPath):
     '''将tile size写入csv文件中'''
-    header = []
+    header = ["segment", "tile"]
     for i in range(len(BITRATE_KBPS)):
         header.append("level_" + str(i))
 
-    with open(outputPath + "movie360.csv", 'w', newline='') as csvFile:
+    with open(outputPath + VIDEO_NAME + ".csv", 'w', newline='') as csvFile:
         writer = csv.writer(csvFile)
         writer.writerow(header)
         for i in range(len(size_of_tile)):
-            writer.writerow(size_of_tile[i])
-            writer.writerow()
+            for j in range(len(size_of_tile[0])):
+                tmp = [i, j]
+                tmp += size_of_tile[i][j]
+                # tmp.append(size_of_tile[i][j][:])
+                writer.writerow(tmp)
+            # writer.writerow()
+    return
 
 
 # todo：改成命令行输入的形式
 def main():
-    # 1. 读取目录下的所有文件的大小
-    inputPath = "dash_file"
-    size_of_tile = get_tile_size(inputPath)
-    print(size_of_tile)
-    # 2. 生成含video trace的movie360.json文件
+    # 1. 分别读取目录下的所有文件的大小
+    inputPath_low = "F:\\360-degree video streaming\dash-360\src\main\scripts\\" + VIDEO_NAME + "\output_low"
+    # inputPath_low = "F:\\360-degree video streaming\dash-360\src\main\scripts\\1-4-Conan Weird Al\output_low"
+    inputPath_high = "F:\\360-degree video streaming\dash-360\src\main\scripts\\" + VIDEO_NAME + "\output_high"
+    # inputPath_high = "F:\\360-degree video streaming\dash-360\src\main\scripts\\1-4-Conan Weird Al\output_high"
+    print("inputPath_low : ", inputPath_low)
+    print("inputPath_high : ", inputPath_high)
+    size_of_tile_low = get_tile_size(inputPath_low)
+    size_of_tile_high = get_tile_size(inputPath_high)
+
+    # 2. 合并
+    size_of_tile = np.concatenate((size_of_tile_low, size_of_tile_high), axis=2)
+    size_of_tile = size_of_tile.tolist()
+
+    # print(size_of_tile)
+    # 3. 生成含video trace的movie360.json文件
     outputPath = "video_trace/"
     generate_video_trace_json(size_of_tile, outputPath)
-    # todo：增加一个csv文件的写入
+    # 4. 生成csv文件
     generate_video_trace_csv(size_of_tile, outputPath)
 
 
